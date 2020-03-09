ecb6450faf0c408a19e33ada2f9a989bb78a0d83
Warren Harper II <warrenharper2@att.net>
2020-03-09 23:02:58 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,38 +1,27 @@
 require 'pry'
 
-class PigLatinizer
-  def self.translate(phrase)
-      phrase
-        .split(/ /)
-        .map {|word| PigLatin.new(word).translate}
-        .join(" ")
+def piglatinize(word)
+    return word if %w[and an in].include?(word) #one syllable exceptions
+    letters = word.split("")
+    letters.keep_if {|letter| letter != "."}
+    if letters.size > 1
+      until vowel?(letters[0])
+        letters << letters.shift
+      end
+      letters  << "ay"
     end
+    letters.join
+  end
 
-    def initialize(word)
-      @word = word
-    end
+  def to_pig_latin(text)
+    words = text.split(" ")
+    words.map! {|word| piglatinize(word)}
+    words.join(" ")
+  end
 
-    def translate
-      return "#{word}ay" if begins_with_vowel_sound?
-      return "y#{word[0]}ay" if y_after_consonant?
-      rot = begins_with_qu? ? 2 : 1
-      PigLatin.new(word.chars.rotate(rot).join).translate
-    end
-
-    private
-
-    attr_reader :word
-
-    def begins_with_vowel_sound?
-      word.start_with?("a", "e", "i", "o", "u", "xr", "yt")
-    end
-
-    def begins_with_qu?
-      word.start_with?("qu")
-    end
-
-    def y_after_consonant?
-      word.length == 2 && word[1] == "y"
-    end
+  def vowel?(letter)
+    letter.downcase
+    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
+  end
 
 end
