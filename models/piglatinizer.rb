b051952bd04ae1361e6ce974d2827272adcba22d
Warren Harper II <warrenharper2@att.net>
2020-03-09 23:12:08 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,28 +2,37 @@ require 'pry'
 
 class PigLatinizer
 
-def piglatinize(word)
-  return word if %w[and an in].include?(word) #one syllable exceptions
-  letters = word.split("")
-  letters.keep_if {|letter| letter != "."}
-  if letters.size > 1
-    until vowel?(letters[0])
-      letters << letters.shift
-    end
-    letters  << "ay"
+  def piglatinize(thing)
+    pig(thing)
   end
-  letters.join
-end
-
-def to_pig_latin(text)
-  words = text.split(" ")
-  words.map! {|word| piglatinize(word)}
-  words.join(" ")
-end
-
-def vowel?(letter)
-  letter.downcase
-  letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-end
-
-end
+  
+  def to_pig_latin(string)
+    pig(string)
+  end
+  
+  def pig(thing)
+    alpha = ('a'..'z').to_a + ('A'..'Z').to_a
+    vowels = %w[a e i o u] + %w[A E I O U]
+    consonants = alpha - vowels
+  
+    showme = []
+      input = thing.split(' ') 
+        input.each do |word|
+            if word.length < 2
+               showme << word + "way" 
+            elsif vowels.include?(word[0])
+               showme <<  word + "way"
+            elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+               showme <<  word[3..-1] + word[0..2] + 'ay'
+            elsif consonants.include?(word[0]) && consonants.include?(word[1])
+               showme <<  word[2..-1] + word[0..1] + 'ay'
+            elsif consonants.include?(word[0])
+               showme <<  word[1..-1] + word[0] + 'ay'
+            else 
+               showme <<  word + '?'
+            end
+        end 
+    showme.join(', ').gsub(/,/, '')
+  end
+  
+  end 
