b5eedb51b12c633796be40939ecdd497d179f632
Warren Harper II <warrenharper2@att.net>
2020-03-09 23:12:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,21 +5,21 @@ class PigLatinizer
   def piglatinize(thing)
     pig(thing)
   end
-  
+
   def to_pig_latin(string)
     pig(string)
   end
-  
+
   def pig(thing)
     alpha = ('a'..'z').to_a + ('A'..'Z').to_a
     vowels = %w[a e i o u] + %w[A E I O U]
     consonants = alpha - vowels
-  
+
     showme = []
-      input = thing.split(' ') 
+      input = thing.split(' ')
         input.each do |word|
             if word.length < 2
-               showme << word + "way" 
+               showme << word + "way"
             elsif vowels.include?(word[0])
                showme <<  word + "way"
             elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
@@ -28,11 +28,11 @@ class PigLatinizer
                showme <<  word[2..-1] + word[0..1] + 'ay'
             elsif consonants.include?(word[0])
                showme <<  word[1..-1] + word[0] + 'ay'
-            else 
+            else
                showme <<  word + '?'
             end
-        end 
+        end
     showme.join(', ').gsub(/,/, '')
   end
-  
+
   end 
