04992ae52b84f821165d917bd2053fe7bf692262
Warren Harper II <warrenharper2@att.net>
2020-03-09 23:07:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,29 @@
 require 'pry'
 
 class PigLatinizer
-    return word if %w[and an in].include?(word) #one syllable exceptions
-    letters = word.split("")
-    letters.keep_if {|letter| letter != "."}
-    if letters.size > 1
-      until vowel?(letters[0])
-        letters << letters.shift
-      end
-      letters  << "ay"
+  
+def piglatinize(word)
+  return word if %w[and an in].include?(word) #one syllable exceptions
+  letters = word.split("")
+  letters.keep_if {|letter| letter != "."}
+  if letters.size > 1
+    until vowel?(letters[0])
+      letters << letters.shift
     end
-    letters.join
+    letters  << "ay"
   end
+  letters.join
+end
 
-  def to_pig_latin(text)
-    words = text.split(" ")
-    words.map! {|word| piglatinize(word)}
-    words.join(" ")
-  end
+def to_pig_latin(text)
+  words = text.split(" ")
+  words.map! {|word| piglatinize(word)}
+  words.join(" ")
+end
 
-  def vowel?(letter)
-    letter.downcase
-    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-  end
+def vowel?(letter)
+  letter.downcase
+  letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
+end
 
-#end
+end
